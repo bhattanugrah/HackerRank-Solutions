5535a37f5e579f756e02e0994244a9b12c8654ce
bhattanugrah <bhattanugrah@gmail.com>
2020-10-26 00:42:02 +0530

    Equal Stacks: https://www.hackerrank.com/challenges/equal-stacks/problem

---
+++ Equal Stacks.py
@@ -0,0 +1,27 @@
+#Problem Link: https://www.hackerrank.com/challenges/equal-stacks/problem
+
+def equalStacks(h1, h2, h3):
+    h1=h1[::-1]
+    h2=h2[::-1]
+    h3=h3[::-1]
+
+    sum1 = sum(h1)
+    sum2 = sum(h2)
+    sum3 = sum(h3)
+
+    while True:
+        minheight = min(sum1, sum2, sum3)
+
+        if minheight == 0:
+            return 0
+
+        if minheight < sum1:
+            sum1 -= h1.pop()
+        if minheight < sum2:
+            sum2 -= h2.pop()
+        if minheight < sum3:
+            sum3 -= h3.pop()
+
+        if sum1 == sum2 == sum3:
+            return sum1
+        
