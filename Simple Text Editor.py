7035bd2ee3fb00ad3fcc9fbc282dea69fa2678b8
bhattanugrah <bhattanugrah@gmail.com>
2020-10-29 01:20:47 +0530

    Simple Text Editor: https://www.hackerrank.com/challenges/simple-text-editor/problem

---
+++ Simple Text Editor.py
@@ -0,0 +1,19 @@
+#Problem Link: https://www.hackerrank.com/challenges/simple-text-editor/problem
+
+stack = []
+string = ""
+for _ in range(int(input())):
+    t = input().strip()
+
+    if t[0] == '1':
+        stack.append(string)
+        string += t[1]
+    elif t[0] == '2':
+        stack.append(string)
+        string = string[:-int(t[1])]
+
+    elif t[0] == '3':
+        print(string[int(t[1])-1])
+    else:
+        string = stack.pop()
+
