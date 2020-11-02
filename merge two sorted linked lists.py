9b3e14af67d603c9008f5d30c98ee99d8d4ca5ab
bhattanugrah <bhattanugrah@gmail.com>
2020-11-02 23:15:07 +0530

    Merge two sorted Linked Lists:  https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem

--- merge two sorted linked lists.py
+++ merge two sorted linked lists.py
@@ -1,18 +1,16 @@
 #Problem Link: https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem
 
 def mergeLists(head1, head2):
-    if head1 is None and head2 is None:
-        return None
-
-    if head1 is None:
+    if (head1 is None):
         return head2
-
-    if head2 is None:
+    elif (head2 is None):
         return head1
 
-    if head1.data < head2.data:
-        smallerNode = head1.data
-        smallerNode.next = mergeLists(head1.next, head2)
+    if (head1.data <= head2.data):
+        result = head1
+        result.next = mergeLists(head1.next, head2)
     else:
-        smallerNode = head2.data
-        smallerNode.next = mergeLists(head1, head2.next)
+        result = head2
+        result.next = mergeLists(head1, head2.next)
+
+    return result
