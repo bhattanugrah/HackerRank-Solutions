ebc124c6c974e8f70f7cfc3f10d90c2a83d46af8
bhattanugrah <bhattanugrah@gmail.com>
2020-11-11 00:33:26 +0530

    Reverse a Doubly Linked List: https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem

---
+++ Reverse a Doubly Linked List.py
@@ -0,0 +1,9 @@
+#Link to Problem: https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem
+
+def reverse(head):
+    head.prev, head.next = head.next, head.prev
+    if head.prev != None:
+        return reverse(head.prev)
+    else:
+        return head
+    
