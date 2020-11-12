8b69c52a63006abd2fe7d052e3e95617126f54e5
bhattanugrah <bhattanugrah@gmail.com>
2020-11-12 16:37:04 +0530

    Append and Delete: https://www.hackerrank.com/challenges/append-and-delete/problem

---
+++ append and delete.py
@@ -0,0 +1,18 @@
+#Problem Link: https://www.hackerrank.com/challenges/append-and-delete/problem
+
+def appendAndDelete(s, t, k):
+    count = 0
+
+    for i, j in zip (s, t):
+        if i == j:
+            count+=1
+        else:
+            break
+
+    total_length = len(s) + len(t)
+
+    if total_length <= 2*count+k and total_length%2 == k%2 or total_length<k:
+        return "Yes"
+    else:
+        return "No"
+    
